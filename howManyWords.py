1c9fa17e6da8aceec59d7c26b671b139fdc152a1
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-10 08:49:31 -0400

    Updated from Brython Server: 5/10/2018 8:49:30 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,3 +1,13 @@
 #Ray Tso
 #5/10/18
 #howManyWords.py
+
+file=open('engmix.txt')
+words=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+numWords = 0
+for line in file:
+    words[len(line)]+=1
+print(words)
+    
+
