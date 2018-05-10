b1ce566aa7a58c8c8a0b7f664f2beda513dca4fa
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-10 08:09:29 -0400

    Updated from Brython Server: 5/10/2018 8:09:28 AM

--- lastWordDemo.py
+++ lastWordDemo.py
@@ -1,1 +1,12 @@
+#Ray Tso
+#5/10/18
+#lastwordDemo.py
 
+file=open('filesDemo.py')
+
+for line in file:
+    words=line.split()
+    if len(words)>0:
+        print(words[-1])
+    else:
+        print()
