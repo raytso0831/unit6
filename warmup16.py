8075ffc586becea433c4da67b6ad6d7c2f52937b
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-11 08:41:37 -0400

    Updated from Brython Server: 5/11/2018 8:41:36 AM

--- warmup16.py
+++ warmup16.py
@@ -2,14 +2,12 @@
 #5/11/18
 #warmup16.py
 
-numWords = 0
 file = open('engmix.txt')
 for word in file:
     word=word.strip()
-    if len(file)<0 and word[0]=='r'and word[-1]=='t':
+    if len(word)<0 and word[0]=='r'and word[-1]=='t':
         print(word.strip())
-    numWords += 1
-print(numWords)
+   
 
 
     
