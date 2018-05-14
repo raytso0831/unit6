ffc9281629179093ff7e41661402a4276e69e429
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-14 08:31:26 -0400

    Updated from Brython Server: 5/14/2018 8:31:26 AM

--- hw6.py
+++ hw6.py
@@ -1,1 +1,15 @@
+#Ray Tso
+#5/14/18
+#hw6.py
 
+file=open('engmix.txt')
+words=input('enter a word:')
+present=False
+
+for list in file:
+    if words==list.strip():
+        present=True
+        print(words,'is in the list')
+  
+if present==False:
+    print(words,'is not in the list')
