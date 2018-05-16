4697619c12533e7191ba9496e49a35bdba528494
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-16 08:38:58 -0400

    Updated from Brython Server: 5/16/2018 8:38:58 AM

--- reverse.py
+++ reverse.py
@@ -1,1 +1,14 @@
+#Ray Tso
+#5/16/18
+#reverse.py
 
+file=open('lastWordDemo.py')
+
+list=[]
+for line in file:
+    line=line.strip()
+    list.append(line)
+list.reverse()
+
+for l in list:
+    print(l)
