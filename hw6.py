c8df63579196b58ca4981d04e96925f6aaa8b87a
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-16 08:18:05 -0400

    Updated from Brython Server: 5/16/2018 8:18:04 AM

--- hw6.py
+++ hw6.py
@@ -2,6 +2,7 @@
 #5/14/18
 #hw6.py
 
+'''
 file=open('engmix.txt')
 words=input('enter a word:')
 present=False
@@ -13,3 +14,27 @@ for list in file:
   
 if present==False:
     print(words,'is not in the list')
+    '''
+    
+'''
+file=open('engmix.txt')
+num=int(input('enter a number'))
+L=[]
+for line in file:
+    line=line.strip
+    L.append(line)
+print(L[num-1])
+'''
+'''
+file=open('lastWordDemo.py')
+for line in file:
+    print(line.strip()+'!')
+'''
+file=open('engmix.txt')
+letter=input('enter a letter:')
+mostchar=''
+for line in file:
+    line=line.strip()
+    if line.count(letter)>most.char.count(letter):
+        mostchar=line
+print(mostchar)
