0fee26430c47616cbccc2eea511dc99877d9b06a
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-17 08:49:28 -0400

    Updated from Brython Server: 5/17/2018 8:49:27 AM

--- palindromes.py
+++ palindromes.py
@@ -3,3 +3,14 @@
 #palindromes.py
 
 file=open('engmix.txt')
+
+for line in file:
+    line=line.strip()
+    back=''
+    for ch in line:
+        back=ch+back
+    if line==back:
+        print(line)
+        
+        
+    
