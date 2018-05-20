c20727df43d9536cea480ab60360a9144bac3c5b
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-20 17:42:55 -0400

    Updated from Brython Server: 5/20/2018 5:42:55 PM

--- practiceQuiz6.py
+++ practiceQuiz6.py
@@ -8,23 +8,58 @@ for line in file:
     if line.count('c')==3 and line.count('p')==2:
         print(line)
         '''
-
-#program 2
 '''
+#program 2
+
 file=open('engmix.txt')
 numWord=0
 for line in file:
     if 'r'in line:
         numWord +=1
-print(numWord)
+print('There are' ,numWord, 'words with the letter r')
 '''
 
+
 #program 3
+'''
 file=open('engmix.txt')
 numWord=int(input('Enter a number'))
 for line in file:
     if len(line.strip())==numWord:
         print(line.strip())
         break
+        '''
+'''
+#program 4
+file=open('engmix.txt')
+numWord=0
+letter=input('enter a letter')
+for line in file:
+    if letter not in line:
+        numWord +=1
+print('There is',numWord,'words without the letter' ,letter, )
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
