b9ec8be68e2dbabc1a8dbba3cb5f9e9459fb3287
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-20 17:31:59 -0400

    Updated from Brython Server: 5/20/2018 5:31:58 PM

--- practiceQuiz6.py
+++ practiceQuiz6.py
@@ -2,8 +2,18 @@
 #practiceQuiz6.py
 
 #program 1
+'''
 file=open('engmix.txt')
 for line in file:
     if line.count('c')==3 and line.count('p')==2:
         print(line)
+        '''
+
+#program 2
+file=open('engmix.txt')
+numWord=0
+for line in file:
+    if 'r'in line:
+        numWord +=1
+print(numWord)
 
