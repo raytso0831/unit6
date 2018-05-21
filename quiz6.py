1ed23323c28855b5c7a2a344bbd9ced6606a6513
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-21 08:08:58 -0400

    Updated from Brython Server: 5/21/2018 8:08:57 AM

--- quiz6.py
+++ quiz6.py
@@ -2,4 +2,22 @@
 #5/21/18
 #quiz6.py
 
+'''
+#program 1
+file=open('engmix.txt')
+letter=input('enter a letter')
+for line in file:
+    if line.count(letter)==4:
+        print(line.strip())
+        '''
+'''
+#program 2
+file=open('engmix.txt')
+for line in file:
+    if line[0]==line[4]==line[8]:
+        print(line.strip())
+        '''
+#program 3
+file=open('engmix.txt')
+
 
