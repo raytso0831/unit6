17c17baca1f97cf847fd63e1daf540159d855b95
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-21 08:16:04 -0400

    Updated from Brython Server: 5/21/2018 8:16:04 AM

--- quiz6.py
+++ quiz6.py
@@ -10,14 +10,26 @@ for line in file:
     if line.count(letter)==4:
         print(line.strip())
         '''
+
 '''
 #program 2
 file=open('engmix.txt')
 for line in file:
-    if line[0]==line[4]==line[8]:
+    if line[0]==line[4] and line[0]==line[8] and line[4]==line[8]:
         print(line.strip())
         '''
+'''
 #program 3
 file=open('engmix.txt')
+letter=input('eneter a letter')
+numWord=int(input('Enter a number'))
+for line in file:
+    if len(line.strip())==numWord and  letter in line:
+        print(line.strip())
+        break
+        '''
+#program 4
+file=open('engmix.txt')
+
 
 
